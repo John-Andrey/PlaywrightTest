016e64193e0f0d768bcb70ce309e7485a35b9b17
Andrei <andrebest94@gmail.com>
2024-10-01 16:57:12 +0200

    commit

--- tests/Homepagetest.spec.js
+++ tests/Homepagetest.spec.js
@@ -1,14 +1,15 @@
-const {test, expect} =require('@playwright/test');
+const { test, expect} = require('@playwright/test');
 //i have to pass 2 parameters, Home page and anonymous function ()
-test('Home page',async (page)=>{
+test('Home Page',async ({page})=>{
 
     await page.goto('https://demoblaze.com/index.html');
     const pageTitle=page.title();
     console.log('Page title is:', pageTitle);
+
     await expect(page).toHaveTitle('STORE');
      
-    const pageURL=PageTransitionEvent.url();
-    console.log('Page URL is :', pageURL)
+    const pageURL=page.url();
+    console.log('Page URL is :', pageURL);
     expect(page).toHaveURL('https://demoblaze.com/index.html');
 
     await page.close();
